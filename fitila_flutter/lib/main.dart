db0932bebd7ec54d4eb916effb2afc5527887282
zimesongbian007 <zimesongbian007@gmail.com>
2026-09-14 14:53:23 +0100

    feat(web-parity): wire full classe web corpus into Flutter

--- fitila_flutter/lib/main.dart
+++ fitila_flutter/lib/main.dart
@@ -13,6 +13,7 @@ import 'core/fitila_backend.dart';
 import 'core/fitila_media.dart';
 import 'core/foncier_rag.dart';
 import 'core/signature_theme.dart';
+import 'core/web_parity_models.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
