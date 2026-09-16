5c29dd529fbfc1c6801f52effa00ba9d3767436f
zimesongbian007 <zimesongbian007@gmail.com>
2026-09-16 13:48:52 +0100

    test(feed): allow immersive Fil Fitila surfaces

--- fitila_flutter/test/mobile_widget_test.dart
+++ fitila_flutter/test/mobile_widget_test.dart
@@ -20,7 +20,7 @@ void main() {
     await tester.tap(find.text('Se connecter'));
     await tester.pumpAndSettle();
 
-    expect(find.text('Fil Fitila'), findsOneWidget);
+    expect(find.text('Fil Fitila'), findsWidgets);
     expect(tester.takeException(), isNull);
   });
 }
