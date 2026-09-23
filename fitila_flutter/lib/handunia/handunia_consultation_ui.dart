3ef1b92c51e48858a18dd7e7c91cbb657ffa320e
zimesongbian007 <zimesongbian007@gmail.com>
2026-09-23 09:07:52 +0100

    fix(handunia): expose voice-help action to accessibility

--- fitila_flutter/lib/handunia/handunia_consultation_ui.dart
+++ fitila_flutter/lib/handunia/handunia_consultation_ui.dart
@@ -1650,7 +1650,6 @@ class _HanduniaFilViewState extends State<HanduniaFilView> {
                 Semantics(
                   button: true,
                   label: 'Aider à compléter cette mémoire par la voix',
-                  excludeSemantics: true,
                   child: SizedBox(
                     width: 58,
                     height: 58,
